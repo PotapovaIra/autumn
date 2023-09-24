29322fa5b6925b10ae60147a45454ee8c2e54457
PotapovaIra <127214054+PotapovaIra@users.noreply.github.com>
2023-09-24 19:47:27 +0300

    Update 3_2.cpp
    исправлено

--- 3_2.cpp
+++ 3_2.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
 #include <vector>
 
+int partition(std::vector<int>& mas, int low, int high);
+void quicksort(std::vector<int>& mas, int low, int high);
+
+
 int partition(std::vector<int>& mas, int low, int high) {
-    auto pivot = mas[(low + high) / 2];
-    auto i = low;
-    auto j = high;
-    while (true) {
-        while (mas[i] <= pivot) {
+    auto pivot = mas[low];
+    auto i = low - 1;
+    auto j = high + 1;
+    while (i < j) {
+        do {
             i++;
-        }
-        while (mas[j] >= pivot) {
+        } while (mas[i] < pivot);
+        do {
             j--;
+        } while (mas[j] > pivot);
+        if (i < j) {
+            std::swap(mas[i], mas[j]);
         }
-        if (i >= j) {
-            return j;
-        }
-        std::swap(mas[i++], mas[j--]);
     }
+    return j;
 }
 
 void quicksort(std::vector<int>& mas, int low, int high) {
-    if (low < high) {
-        auto p = partition(mas, low, high);
-        quicksort(mas, low, p - 1);
-        quicksort(mas, p + 1, high);
+    if (low < 0 || high >= mas.size() || low >= high) {
+        return;
     }
+    auto p = partition(mas, low, high);
+    quicksort(mas, low, p);
+    quicksort(mas, p + 1, high);
 }
 int main() {
-    std::vector<int> mas = { 3,1,4,1,5,9,2,6,5,3,5 };
+    std::vector<int> mas = { 1,82,8,18,28,45,4,5,90,45 };
     quicksort(mas, 0, mas.size() - 1);
 
     for (const auto& el : mas) {
