956afa5054dfcfa6dcb5a665e82a293606b11462
PotapovaIra <127214054+PotapovaIra@users.noreply.github.com>
2023-11-06 17:53:36 +0300

    Upd

--- hw_8/n_1.cpp
+++ hw_8/n_1.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 #include <exception>
-
+#include <vector>
+#include <cmath>
 
 void test1() {
-    throw std::runtime_error("Runtime error ");
+    std::vector<int> v(5);
+    v(10) = 3;
 }
 
 void test2() {
-    throw std::logic_error("Logic error");
+    int a = 5, b = 0;
+    int c = a / b;
 }
 
 void test3() {
-    int* ptr = nullptr;
-    *ptr = 10;
+    double x = -1;
+    double y = sqrt(x);
 }
 
-
 void test4() {
-    throw std::out_of_range("Out of range error");
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    int& r = arr[10];
 }
 
 int main() {
     try {
         test1();
     }
-    catch (std::exception& e) {
+    catch (std::out_of_range& e) {
         std::cout << "Exception catch: " << e.what() << std::endl;
     }
     try {
         test2();
     }
-    catch (std::exception& e) {
+    catch (std::logic_error& e) {
         std::cout << "Exception catch: " << e.what() << std::endl;
     }
     try {
         test3();
     }
-    catch (std::exception& e) {
+    catch (std::domain_error& e) {
         std::cout << "Exception catch: " << e.what() << std::endl;
     }
     try {
         test4();
     }
-    catch (std::exception& e) {
+    catch (std::out_of_range& e) {
         std::cout << "Exception catch: " << e.what() << std::endl;
     }
     return 0;
