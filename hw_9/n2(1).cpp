7cbb2cf48ddc2ab334ef777f9cc2662bce241e50
PotapovaIra <127214054+PotapovaIra@users.noreply.github.com>
2023-11-14 18:52:35 +0300

    main

--- hw_9/n2(1).cpp
+++ hw_9/n2(1).cpp
@@ -86,3 +86,28 @@ template<typename T, std::size_t N>
 void array<T, N>::swap(array& info) noexcept {
 	std::swap(arr, info.arr);
 }
+
+int main() {
+	array<int, 5> arr1;
+	array<int, 5> arr2;
+
+	arr1.front() = 10;
+	arr1.back() = 20;
+
+	//operator[] 
+	arr1[2] = 15;
+
+	// size
+	std::cout << "Size of arr1: " << arr1.size() << std::endl;
+
+	// swap 
+	arr1.swap(arr2);
+
+	// empty 
+	std::cout << "arr empty? " << arr1.empty() << std::endl;
+
+	// max_size 
+	std::cout << "Max size of arr1: " << arr1.max_size() << std::endl;
+
+	return 0;
+}
