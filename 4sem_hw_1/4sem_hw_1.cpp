1f279907361c4bacd7d5b07c263dfecdacd564e0
PotapovaIra <127214054+PotapovaIra@users.noreply.github.com>
2024-02-09 14:38:52 +0300

    Update 4sem_hw_1.cpp

--- 4sem_hw_1/4sem_hw_1.cpp
+++ 4sem_hw_1/4sem_hw_1.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
+    std::string user_name = "user"; // Определить переменную.
+    std::cout << "Hello, " << user_name << "!" << std::endl;
     std::cout << "Hello World!\n";
 }
 int main()
 {
     return 0; 
+
 }
