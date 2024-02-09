06dae61357c5aaf4a2cf94255dc5c1b3ac57e0f5
PotapovaIra <potapovairina05@mail.ru>
2024-02-09 15:03:07 +0300

    changed name

---
+++ 4sem_hw_1/task1_commit.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string user_name = "user"; // Определить переменную.
+    std::cout << "Hello, " << user_name << "!" << std::endl;
+    std::cout << "Hello World!\n";
+}
+int main()
+{
+    return 0; 
+
+}
