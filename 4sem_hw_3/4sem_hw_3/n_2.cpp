52ef40c858711addafacf7073f6ce6772dea1417
PotapovaIra <potapovairina05@mail.ru>
2024-03-01 16:23:58 +0300

    hash_code

--- 4sem_hw_3/4sem_hw_3/n_2.cpp
+++ 4sem_hw_3/4sem_hw_3/n_2.cpp
@@ -14,11 +14,27 @@ std::size_t hash(const char* s)
 	return h % 128;
 }
 struct Fields {
-	int intField;
-	double doubleField;
-	std::string stringField;
+	int integer;
+	double doubl;
+	std::string str;
 };
+int main()
+{
+	std::unordered_set<std::size_t> hashCodes;
 
+	const auto numExamples = 1000; //examples
+	for (int i = 0; i < numExamples; i++)
+	{
+		Fields field;
+		field.integer = rand();
+		//field.doubl = ///
+		field.str = std::to_string(i);
+
+		//count hash-code for examples
+		std::size_t hashCode = hash(field.str.c_str());
+		hashCodes.insert(hashCode);
+	}
+}
 //int main(int argc, char** argv)
 //{
 //	std::cout << hash("ivanov") << std::endl;
