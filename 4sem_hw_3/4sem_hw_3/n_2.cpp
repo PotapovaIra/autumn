0229199d60b28debd179716a6ea104cb1645ce5f
PotapovaIra <potapovairina05@mail.ru>
2024-03-04 19:03:33 +0300

    по итогу все равно получился какой-то бред

--- 4sem_hw_3/4sem_hw_3/n_2.cpp
+++ 4sem_hw_3/4sem_hw_3/n_2.cpp
@@ -2,6 +2,62 @@
 #include <string>
 #include <unordered_set>
 #include <fstream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <boost/functional/hash.hpp>
+
+#include <vector>
+
+struct Fields {
+    double int1;
+    double doubl1;
+    double doubl2;
+};
+
+int main(int argc, char** argv)
+{
+    // better to be as more as possible
+    constexpr std::size_t size = 100'000;
+    constexpr auto min_int = -100;
+    constexpr auto max_int = 100;
+
+    auto gen = std::mt19937_64(std::chrono::system_clock::now().time_since_epoch().count());
+    auto dist = std::uniform_real_distribution(min_int, max_int);
+
+    auto vec = std::vector<double>(size, 0.0);
+    std::generate(std::begin(vec), std::end(vec), [&gen, &dist]() { return dist(gen); });
+
+    std::unordered_set<std::size_t> hashCodes;
+    int numCollisions = 0;
+    const auto numExamples = 1000;
+
+    for (int i = 0; i < numExamples; i++)
+    {
+        Fields field;
+        field.int1 = std::uniform_int_distribution<int>(-100, 100)(gen);
+        field.doubl1 = std::uniform_real_distribution<double>(-100.0, 100.0)(gen);
+        field.doubl2 = std::uniform_real_distribution<double>(-100.0, 100.0)(gen);
+
+        std::size_t hashCode = 0;
+        boost::hash_combine(hashCode, field.int1);
+        boost::hash_combine(hashCode, field.doubl1);
+        boost::hash_combine(hashCode, field.doubl2);
+
+        int prevSize = hashCodes.size();
+        hashCodes.insert(hashCode);
+        int current = hashCodes.size();
+
+
+        std::cout << "collisions: " << prevSize - current << "examples: " << current << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
 
 //std::size_t hash(const char* s)
 //{
@@ -14,41 +70,6 @@
 //
 //	return h % 128;
 //}
-struct Fields {
-	int integer;
-	double doubl;
-	std::string str;
-};
-int main()
-{
-	std::unordered_set<std::size_t> hashCodes;
-	//hash_func
-	const auto numExamples = 1000; //examples
-	for (int i = 0; i < numExamples; i++)
-	{
-		Fields field;
-		field.integer = rand();
-		field.doubl = static_cast<double>(rand()) / RAND_MAX;
-		field.str = std::to_string(i);
-
-		//count hash-code for examples
-		std::size_t hashCode = hash(field.str.c_str());
-		hashCodes.insert(hashCode);
-	}
-
-	std::ofstream out("hash_codes.txt");
-	for (auto code : hashCodes)
-	{
-		out << code << std::endl;
-	}
-	out.close();
-	std::cout << "Succesfully saved" << std::endl;
-
-	return 0;
-}
-
-
-
 //int main(int argc, char** argv)
 //{
 //	std::cout << hash("ivanov") << std::endl;
