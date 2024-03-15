276232072495f35e36c05f5217ce6292690b702d
PotapovaIra <potapovairina05@mail.ru>
2024-03-15 20:06:57 +0300

    added alphabet

--- 4sem_hw_5/4sem_hw_5/n2_transliteration.cpp
+++ 4sem_hw_5/4sem_hw_5/n2_transliteration.cpp
@@ -6,7 +6,13 @@
 
 std::wstring translit(const std::wstring& input) {
     const std::unordered_map<wchar_t, std::wstring> translit = { // had to change to wstring because of error
-        { L'À', L"A" }, { L'Á', L"B" }, { L'Â', L"V" }, { L'Ã', L"G" }, //for example
+        {L'א', L"a"}, {L'ב', L"b"}, {L'ג', L"v"}, {L'ד', L"g"}, {L'ה', L"d"},
+    {L'ו', L"e"}, {L'¸', L"yo"}, {L'ז', L"zh"}, {L'ח', L"z"}, {L'ט', L"i"},
+    {L'י', L"y"}, {L'ך', L"k"}, {L'כ', L"l"}, {L'ל', L"m"}, {L'ם', L"n"},
+    {L'מ', L"o"}, {L'ן', L"p"}, {L'נ', L"r"}, {L'ס', L"s"}, {L'ע', L"t"},
+    {L'ף', L"u"}, {L'פ', L"f"}, {L'ץ', L"kh"}, {L'צ', L"ts"}, {L'ק', L"ch"},
+    {L'ר', L"sh"}, {L'ש', L"shch"}, {L'ת', L""}, {L'û', L"y"}, {L'ü', L""},
+    {L'‎', L"e"}, {L'‏', L"yu"}, {L'ÿ', L"ya"}, //changed for alphabet, not capital 
     };
 
     std::wstring result; //used algorythm from habr
@@ -43,10 +49,10 @@ std::string convert_utf_to_locale(const std::string& string) //copies from semin
     return boost::locale::conv::from_utf <char>(string, locale);
 }
 int main() {
-    std::string str = "ÀÁÂÃ";
+    std::string str = "אבגדהו¸זחט¸ךכלםמןנסע";
 
     std::string utf8 = convert_locale_to_utf(str);//from windows-1251 to utf-8
-    std::cout << utf8 << std::endl;
+    //std::cout << utf8 << std::endl;
     std::wstring utf32 = boost::locale::conv::utf_to_utf<wchar_t>(utf8);//from utf_8 to utf-32
 
     std::wstring transliterate = translit(utf32);//transliterate
