4594144a1370e6bfd17dd36d043fcee081bba865
PotapovaIra <potapovairina05@mail.ru>
2024-03-15 19:55:20 +0300

    main function

--- 4sem_hw_5/4sem_hw_5/n2_transliteration.cpp
+++ 4sem_hw_5/4sem_hw_5/n2_transliteration.cpp
@@ -43,6 +43,16 @@ std::string convert_utf_to_locale(const std::string& string) //copies from semin
     return boost::locale::conv::from_utf <char>(string, locale);
 }
 int main() {
+    std::string str = "¿¡¬√";
 
+    std::string utf8 = convert_locale_to_utf(str);//from windows-1251 to utf-8
+    std::cout << utf8 << std::endl;
+    std::wstring utf32 = boost::locale::conv::utf_to_utf<wchar_t>(utf8);//from utf_8 to utf-32
+
+    std::wstring transliterate = translit(utf32);//transliterate
+
+    std::string utf8_transliterate = boost::locale::conv::utf_to_utf<char>(transliterate);//to utf-8
+    std::string result = convert_utf_to_locale(utf8_transliterate);//urf to windows-1251
+    std::cout << result << std::endl;
     return 0;
 }
